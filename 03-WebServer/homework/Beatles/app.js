153aecf79a5f2376c3c02f9890a8a7de7752a1a9
unknown <estiven.carmona@udea.edu.co>
2022-11-03 18:00:03 -0500

    Homework incompleta - Falta showByName

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,64 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http.createServer((req, res)=>{
+  if(req.url === '/'){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+    html = html.replace('{john}', beatles[0].name)
+    html = html.replace('{paul}', beatles[1].name)
+    html = html.replace('{george}', beatles[2].name)
+    html = html.replace('{richard}', beatles[3].name)
+    html = html.replace('{textJohn}', beatles[0].birthdate)
+    html = html.replace('{textPaul}', beatles[1].birthdate)
+    html = html.replace('{textGeorge}', beatles[2].birthdate)
+    html = html.replace('{textRichard}', beatles[3].birthdate)
+    html = html.replace('{imgJohn}', beatles[0].profilePic)
+    html = html.replace('{imgPaul}', beatles[1].profilePic)
+    html = html.replace('{imgGeorge}', beatles[2].profilePic)
+    html = html.replace('{imgRichard}', beatles[3].profilePic)
+    res.end(html)
+  } 
+  else if(req.url === '/api'){
+    res.writeHead(200, {'Content-Type':'application/json'})
+    res.end(JSON.stringify(beatles))
+  }
+  else if (req.url==='/api/John%20Lennon'){
+    var html = fs.readFileSync(__dirname + '/beatle.html', 'utf8');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    html = html.replace('{name}', beatles[0].name)
+    html = html.replace('{text}', beatles[0].birthdate)
+    var longitud = beatles[0].profilePic.split('')
+    html = html.replace('{imgJohn}', beatles[0].profilePic)
+    res.end(html)
+  }
+  else if (req.url==='/api/Paul%20McCartney'){
+    var html = fs.readFileSync(__dirname + '/beatle.html', 'utf8');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    html = html.replace('{name}', beatles[1].name)
+    html = html.replace('{text}', beatles[1].birthdate)
+    html = html.replace('{imgPaul}', beatles[1].profilePic)
+    res.end(html)
+  }
+  else if (req.url==='/api/George%20Harrison'){
+    var html = fs.readFileSync(__dirname + '/beatle.html', 'utf8');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    html = html.replace('{name}', beatles[2].name)
+    html = html.replace('{text}', beatles[2].birthdate)
+    html = html.replace('{imgGeorge}', beatles[2].profilePic)
+    res.end(html)
+  }
+  else if (req.url==='/api/Richard%20Starkey'){
+    var html = fs.readFileSync(__dirname + '/beatle.html', 'utf8');
+    res.writeHead(200, {'Content-Type':'text/html'})
+    html = html.replace('{name}', beatles[3].name)
+    html = html.replace('{text}', beatles[3].birthdate)
+    html = html.replace('{imgRichard}', beatles[3].profilePic)
+    res.end(html)
+  }
+  else{
+    res.writeHead(404); //Ponemos el status del response a 404: Not Found
+    res.end(); //No devolvemos nada más que el estado.
+    }
+}).listen(1337, '127.0.0.1');
