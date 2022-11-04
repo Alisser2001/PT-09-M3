69a298ca1f4347298e57cec5eecfc8a82bd43e20
unknown <estiven.carmona@udea.edu.co>
2022-11-04 12:35:40 -0500

    03-WebServer Completed

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,54 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer((req, res)=>{
+    if (req.url==='/arcoiris%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/arcoiris_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/badboy%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/badboy_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/code%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/code_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/arcoiris%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/arcoiris_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/resaca%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/resaca_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/retrato%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/retrato_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+    else if (req.url==='/sexy%20doge'){
+        res.writeHead(200, {'Content-Type':'image/jpg'});
+        fs.readFile('./images/sexy_doge.jpg', (err, data)=>{
+            if (!err) return res.end(data)
+            throw new Error(err)
+        })
+    }
+}).listen(1337, '127.0.0.1')
