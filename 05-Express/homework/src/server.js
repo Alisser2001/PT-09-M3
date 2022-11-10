62c27e2188645c8238240e9222de25096e5b7c4c
unknown <estiven.carmona@udea.edu.co>
2022-11-10 18:04:48 -0500

    05-Express Completed

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -1,17 +1,47 @@
 // const bodyParser = require("body-parser");
 const express = require("express");
+const { 
+    createPost,
+    createPostAuthor,
+    readPost,
+    readPostauthor,
+    readPostAuthorTitle,
+    deletePost,
+    updatePost,
+    deletePostsAuthor,
+    posts 
+} = require('../routes/post')
 
-const STATUS_USER_ERROR = 422;
 
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
-const posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
+
+//FRONT SIMULADO
+// axios.post('/post', {
+//     firstName: 'Bruce',
+//     lastName: 'Wayne'
+//   })
+  
 
 // TODO: your code to handle requests
 
+server.route('/posts')
+    .post(createPost)
+    .get(readPost)
+    .put(updatePost)
+    .delete(deletePost)
+
+server.get('/posts/:author', readPostauthor);
+
+server.get('/posts/:author/:title', readPostAuthorTitle);
+
+server.post('/posts/author/:author', createPostAuthor);
+
+
+server.delete('/author', deletePostsAuthor);
 
 module.exports = { posts, server };
